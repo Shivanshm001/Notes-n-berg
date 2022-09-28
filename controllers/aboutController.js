90b93c6dcb1cfeb941d3c259008423c6164ceab1
Shivanshm001 <Shivanshmishra084@gmail.com>
2022-09-29 00:46:43 +0530

    Added emailjs, feedback form funtioning, changed UI in forms

--- controllers/aboutController.js
+++ controllers/aboutController.js
@@ -1,17 +1,37 @@
-const nodemailer = require('nodemailer');
-const getAboutPage = (req,res,next)=>{
-    res.status(200).send("About");
-    next()
+const axios = require('axios');
+
+async function sendEmail() {
+
 }
 
-const sendFeedback = (req,res,next)=>{
-    const {email,subject,msg}  = req.body;
-    console.log(email,subject,msg);
-    res.status(200).json({success: true});
+const sendFeedback = async (req, res, next) => {
+    const { name, subject, msg } = req.body;
+    console.log(name, subject, msg);
+    //Emailjs
+    let emailData = {
+        service_id: process.env.EMAILJS_SERVICE_KEY,
+        template_id: process.env.EMAILJS_TEMPLATE_KEY,
+        user_id: process.env.EMAILJS_PUBLIC_KEY,
+        accessToken : process.env.EMAILJS_PRIVATE_KEY,
+        template_params: {
+            from_name: name,
+            to_name: 'Shivansh mishra',
+            subject: subject,
+            message: msg,
+        }
+    }
+    try {
+        const { data } = await axios.post('https://api.emailjs.com/api/v1.0/email/send', emailData);
+        console.log(`Axios response data: ${data}`);
+        res.status(200).json({ success: true });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({success: false});
+    }
+    //Emailjs end
     next();
 }
 
 module.exports = {
-    getAboutPage,
     sendFeedback
 }
--- routes/about.js
+++ routes/about.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const {
-    getAboutPage,
     sendFeedback
 } = require('../controllers/aboutController')
 
